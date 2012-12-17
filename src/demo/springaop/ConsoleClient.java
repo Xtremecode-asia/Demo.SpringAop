edfd78a2237b7187816e151c58fe1d5bc8e62c89
Wendy Sanarwanto <saintc0d3r@gmail.com>
2012-12-17 23:16:57 +0800

    -  Small improvement on the console client. - Removed @Around advice and replaced with @Before & @After

--- src/demo/springaop/ConsoleClient.java
+++ src/demo/springaop/ConsoleClient.java
@@ -6,7 +6,7 @@ import demo.springaop.service.messages.DigestResponse;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-//import java.io.Console;
+import java.io.Console;
 
 /**
  * Created by Xtremecodes.asia @2012
@@ -16,17 +16,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class ConsoleClient {
     public static void main(String[] args){
-//        Console console = System.console();
-//        if (console == null){
-//            System.err.println("No console.");
-//            System.exit(1);
-//        }
-
         ApplicationContext appContext = new ClassPathXmlApplicationContext("config.xml");
         IDigestService digestService = appContext.getBean(IDigestService.class);
         if (digestService != null){
-            //String target = console.readLine("Enter any plain string:");
-            String target = "Hello World";
+            Console console = System.console();
+            String target;
+            if (console == null){
+                target = "Hello World";
+            }
+            else{
+                target = console.readLine("Enter any plain string:");
+            }
             DigestResponse response = digestService.digest(new DigestRequest(target, "SHA-512"));
             System.out.println( String.format("Digested '%s' is '%s'", target,response.getResult()));
         }
---
+++ src/demo/springaop/aspect/ServiceCallsLoggerAspect.java
@@ -0,0 +1,42 @@
+package demo.springaop.aspect;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Xtremecodes.asia @2012
+ * User: saintc0d3r@gmail.com
+ * Date: 12/16/12
+ * Time: 8:11 PM
+ */
+
+@Aspect
+@Component
+public class ServiceCallsLoggerAspect {
+    private static final String ON_BEFORE_LOG_MESSAGE_FORMAT = "Start calling '%s' method at %s";
+    private static final String ON_AFTER_LOG_MESSAGE_FORMAT = "Finished calling '%s' method at %s";
+
+    private Log logger;
+    private String methodsName;
+
+    @Before("execution(public * demo.springaop.service.*.*(..))")
+    public void logBeforeServiceCall(JoinPoint joinPoint) {
+        Class methodOwner = joinPoint.getSignature().getDeclaringType();
+        logger = LogFactory.getLog(methodOwner);
+        methodsName = String.format("%s.%s", methodOwner.getName(), joinPoint.getSignature().getName());
+        // Log the timestamp the beginning before actually calling the method
+        logger.info(String.format(ON_BEFORE_LOG_MESSAGE_FORMAT, methodsName, DateTime.now()));
+    }
+
+    @AfterReturning("execution(public * demo.springaop.service.*.*(..))")
+    public void logAfterServiceCall(){
+        logger.info(String.format(ON_AFTER_LOG_MESSAGE_FORMAT, methodsName, DateTime.now()));
+    }
+
+}
